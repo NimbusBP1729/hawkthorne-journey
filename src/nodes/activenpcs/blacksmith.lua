63f9c5efb46f6e029a92d9829001f8c50985be56
NimbusBP1729 <patjz2@yahoo.com>
2013-03-12 21:00:23 -0400

    updated blacksmith

---
+++ src/nodes/activenpcs/blacksmith.lua
@@ -0,0 +1,16 @@
+return {
+    width = 62,
+    height = 66,
+    animations = {
+        default = {
+            'loop',{'1-4,1'},0.20,
+        }
+    },
+    sounds = {
+        hammer = {
+            state = 'default',
+            position = 2,
+            file = 'sword_hit',
+        }
+    },
+}
